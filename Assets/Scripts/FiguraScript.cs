42c99533628629e3c4ae03c9035dfa338022e8f8
Cesar Zegarra <czegarram@gmail.com>
2013-11-17 18:47:05 -0500

    Scale down world

--- Assets/Scripts/FiguraScript.cs
+++ Assets/Scripts/FiguraScript.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class FiguraScript : MonoBehaviour {
 
+	public const int CONGELADO = 1;
+	public const int MOVIENDOSE = 2;
+
 	public float velocidad;
 	private Vector2 vector_speed;
 	private Vector2 fuerza;
+	private int estado;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +17,8 @@ public class FiguraScript : MonoBehaviour {
 		velocidad *= -1;
 		vector_speed = new Vector2(0, velocidad);
 		rigidbody2D.velocity = vector_speed;
+		estado = MOVIENDOSE;
+
 	}
 	
 	// Update is called once per frame
@@ -22,13 +28,30 @@ public class FiguraScript : MonoBehaviour {
 			if(!Control.getInstancia.stop){
 				rigidbody2D.velocity = Vector2.zero;
 				Control.getInstancia.txt_freeze.guiText.text = "Freeze time!";
+				estado = CONGELADO;
 			}
 			else{
 				rigidbody2D.velocity = vector_speed;
 				Control.getInstancia.txt_freeze.guiText.text = "";
+				estado = MOVIENDOSE;
 			}
 			Control.getInstancia.stop = !Control.getInstancia.stop;
+
+		}else{
+			if( estado == MOVIENDOSE && Input.GetAxis("Horizontal")!=0){
+				if(Input.GetAxis("Horizontal")<0){
+					Debug.Log("izquierda");
+					rigidbody2D.velocity = new Vector2(-3,rigidbody2D.velocity.y);
+				}else if(Input.GetAxis("Horizontal")>0){
+					Debug.Log("derecha");
+					rigidbody2D.velocity = new Vector2(3,rigidbody2D.velocity.y);
+				}
+			}
 		}
+
+
 	}
+
+
 
 }
