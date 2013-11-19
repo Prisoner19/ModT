39ec5865f17fa75ddce2407191d569b1dd985522
Prisoner19 <clz.german@gmail.com>
2013-11-19 12:41:16 -0500

    Asentando figuras

---
+++ Assets/Scripts/RD.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//rigidbody2D.Sleep();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
